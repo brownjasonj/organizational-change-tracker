26819f3b0ee0bc0de441f3004b48ca7922ef6955
Jason Brown <brownjasonj@gmail.com>
2023-01-27 18:30:22 +0100

    finally found the config required to make jest work with commonjs and esm modules.

--- __tests__/organization-shacl-validation.test.ts
+++ __tests__/organization-shacl-validation.test.ts
@@ -4,10 +4,10 @@ import factory from 'rdf-ext';
 
 describe("Stupid Test That should pass!", () => {
   test("Validate Static organization example against organization shape", async () => {
-    const shapes = await loadN3DataSet('src/__tests__/organization-shape.ttl');
-    const data = await loadN3DataSet('src/__tests__/organization.ttl');
+    const shapes = await loadN3DataSet('rdf/ontology/bank-organization.ttl');
+    const data = await loadN3DataSet('rdf/data/bank-organization-example.ttl');
     const validator = new SHACLValidator(shapes, { factory });
     const report = await validator.validate(data);
-    expect(report.conforms).toBe(true)
+    expect(report.conforms).toBe(true);
   });
 });
--- jest.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: 'ts-jest/presets/js-with-ts',
-    testEnvironment: 'node',
-    verbose: true,
-    moduleDirectories: ["node_modules", "src"],
-    
-  };
