335001c2e710eb7d13cf9054079b63c023198438
Jason Brown <brownjasonj@gmail.com>
2023-02-14 11:05:16 +0000

    added a test file with jest type import to solve the tsconfig and vscode relining missing jest types event though they are installed.  mystery

---
+++ __tests__/jest_type_import.test.ts
@@ -0,0 +1,3 @@
+// NB: I couldn't find the right combination of exclude/include in tsconfig.json to make this work, so 
+// I had to add this import and it seems that vscode is just fine.  Absolutely no idea why this works.
+import "@types/jest";
